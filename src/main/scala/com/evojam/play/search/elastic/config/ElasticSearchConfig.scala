399053dbbd3587c4cb6648d446fb2e9526e86c7f
Michael <michael@evojam.com>
2016-02-08 16:27:57 +0100

    Remove ElasticClient
    Use play-elastic4s instead.
    
    Change-Id: Ie0cc9aa720821e557b85aac7683854f0e5a65045

--- src/main/scala/com/evojam/play/search/elastic/config/ElasticSearchConfig.scala
+++
@@ -1,6 +0,0 @@
-package com.evojam.play.search.elastic.config
-
-case class ElasticSearchConfig(addresses: List[(String, Int)], clusterName: Option[String]) {
-  require(addresses != null, "addresses cannot be null")
-  require(clusterName != null, "clusterName cannot be null")
-}
